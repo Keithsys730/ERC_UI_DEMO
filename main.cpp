cfa112e81bcf3b18a49610d3996de41aa0131695
keith <keith@meanwell.com.tw>
2016-06-15 19:19:27 +0800

    Set the screen without window title bar.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
+    a.setOverrideCursor(QCursor(Qt::BlankCursor));
     w.setWindowFlags(Qt::Window|Qt::FramelessWindowHint);
+    //w.showFullScreen();
     w.show();
 
     return a.exec();
