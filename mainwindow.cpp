7f92778162f4278880ea8bfcbe8bd0d15e9dbe0e
keith <keith@meanwell.com.tw>
2016-06-15 20:55:06 +0800

    Add the action for switching the states of burn on and off buttons. Start the time count when burn on button is checked.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QThread>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,33 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_burnOffButton_pressed()
+{
+    if(ui->burnOnButton->isChecked())
+    {
+        ui->burnOnButton->setChecked(false);
+        //ui->burnTimeLabel->setText(QString::number(0));
+    }
+}
+
+void MainWindow::on_burnOnButton_pressed()
+{
+    if(ui->burnOffButton->isChecked())
+    {
+        ui->burnOffButton->setChecked(false);
+        ui->burnTimeLabel->setText(QString::number(0));
+        startBurnTime();
+    }
+}
+
+void MainWindow::startBurnTime()
+{
+    int burnTime=0;
+    for(int temp;temp<100;temp++)
+    {
+        burnTime+=1;
+        ui->burnTimeLabel->setText(QString::number(burnTime));
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_burnOffButton_pressed();
+
+    void on_burnOnButton_pressed();
+
+    void startBurnTime();
+
 private:
     Ui::MainWindow *ui;
 };
