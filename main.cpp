2bdc71e6fed56ed732eb1d82679aa646699dcebb
keith <keith@meanwell.com.tw>
2016-06-17 09:11:53 +0800

    Add the splash screen with the Meanwell logo when starting this program.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSplashScreen>
+#include <QThread>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    /*
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap(":/buttonphotos/Meanwell_2.png"));
+    splash->show();
+
+    Qt::Alignment bottomCenter = Qt::AlignBottom | Qt::AlignCenter;
+    splash->showMessage(QObject::tr("Starting the ERC program..."),bottomCenter, Qt::white);
+    //QThread::msleep(2000);
+    //splash->hide();
+    */
+
     MainWindow w;
 
     a.setOverrideCursor(QCursor(Qt::BlankCursor));
