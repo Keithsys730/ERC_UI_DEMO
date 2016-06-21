9fcf39ad2b250df3d55f66954785ce44619d2874
keith <keith@meanwell.com.tw>
2016-06-21 15:56:51 +0800

    Set the burn on and off buttons as radio property, only one could be selected.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,21 +16,13 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_burnOffButton_pressed()
 {
-    if(ui->burnOnButton->isChecked())
-    {
-        ui->burnOnButton->setChecked(false);
-        //ui->burnTimeLabel->setText(QString::number(0));
-    }
+    ui->burnTimeLabel->setText(QString::number(0));
 }
 
 void MainWindow::on_burnOnButton_pressed()
 {
-    if(ui->burnOffButton->isChecked())
-    {
-        ui->burnOffButton->setChecked(false);
-        ui->burnTimeLabel->setText(QString::number(0));
-        startBurnTime();
-    }
+    ui->burnTimeLabel->setText(QString::number(0));
+    startBurnTime();
 }
 
 void MainWindow::startBurnTime()
